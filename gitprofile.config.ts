fcba0046787a8cb283808ea0de1d4b0691b80df6
27sz <91272647+27sz@users.noreply.github.com>
2024-06-15 14:57:16 +0700

    хз2

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -33,12 +33,12 @@ const CONFIG = {
       // To hide the `External Projects` section, keep it empty.
       projects: [
         {
-          title: 'Project Name',
+          title: '𝗠𝗜𝗥𝗔𝗚𝗘 𝗗𝗨𝗡𝗚𝗘𝗢𝗡',
           description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+            'Dimensions: 200x200',
           imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+            'https://i.imgur.com/SWrC6pB.jpeg',
+          link: 'https://i.imgur.com/SWrC6pB.jpeg',
         },
         {
           title: 'Project Name',
